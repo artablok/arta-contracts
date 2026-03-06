3b2d30036819acedeba5c92e05b9ce871a6b0b7e
artablok <support@artablockchain.in>
2026-03-06 19:58:52 +0300

    Create USDTa.sol

---
+++ contracts/USDTa.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/*
+Arta Blockchain Ecosystem
+Token: USDT.a
+Standard: ART20 / ERC20 compatible
+Repository: https://github.com/artablok/arta-contracts
+*/
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract USDTA is ERC20, Ownable {
+
+    uint256 public constant MAX_SUPPLY = 10000000000 * 10**18;
+
+    constructor() ERC20("USDT.a", "USDT.a") {
+        _mint(msg.sender, MAX_SUPPLY);
+    }
+
+}
